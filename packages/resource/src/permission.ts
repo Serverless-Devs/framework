b88cc445d2fc75e2da4a45c14326c9a5a622ddc0
yuankun.yk <yuankun.yk@alibaba-inc.com>
2021-05-25 15:24:13 +0800

    feat: add permission define

---
+++ packages/resource/src/permission.ts
@@ -0,0 +1,15 @@
+// new RolePolicy("allow-s3-management-policy", {
+//     role: allowS3ManagementRole,
+//     policy: {
+//         Version: "2012-10-17",
+//         Statement: [{
+//             Sid: "AllowS3Management",
+//             Effect: "Allow",
+//             Resource: "*",
+//             Action: "s3:*",
+//         }],
+//     },
+//     'service'
+// }
+// new Policy("allow-s3-management-policy", {})
+// new attcchePolicyToRole("allow-s3-management-policy", {})
