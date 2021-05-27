bcf1f8cb33b946609601e4824b7e2171425af6ec
wb-hfs624703 <wb-hfs624703@alibaba-inc.com>
2021-05-27 11:34:52 +0800

    feat: remove http-route-parser

--- packages/examples/tablestore-http/index.js
+++ packages/examples/tablestore-http/index.js
@@ -1,55 +1,50 @@
 const middy = require('@serverless-devs/dk-core');
 const jsonBodyParser = require('@serverless-devs/http-json-body-parser');
 const formBodyParser = require('@serverless-devs/http-form-body-parser');
-const routeParser = require('@serverless-devs/http-route-parser');
 const tableStorePlugin = require('@serverless-devs/tablestore-initialzer-plugin');
 
 const route = {
-  '/PutRow': {
-    POST: async (request) => {
-      const { tableClient } = request.internal;
-      const { body } = request.req;
-      const params = [
-        { name: body.name },
-        // { age: parseInt(body.age || 0, 10) },
-        { email: body.email },
-        { sex: body.sex },
-      ];
-      if (!tableClient || !body.id) return;
-      console.log(params);
-      await tableClient.putRow({
-        tableName: 'test',
-        condition: {
-          rowExistenceExpectation: 0,
-        },
-        primaryKey: [{ id: body.id }],
-        attributeColumns: params,
-      });
-    },
+  'POST /PutRow': async (request) => {
+    const { tableClient } = request.internal;
+    const { body } = request.req;
+    const params = [
+      { name: body.name },
+      // { age: parseInt(body.age || 0, 10) },
+      { email: body.email },
+      { sex: body.sex },
+    ];
+    if (!tableClient || !body.id) return;
+    console.log(params);
+    const data = await tableClient.putRow({
+      tableName: 'test',
+      condition: {
+        rowExistenceExpectation: 0,
+      },
+      primaryKey: [{ id: body.id }],
+      attributeColumns: params,
+    });
+    return JSON.stringify(data, null, 2);
   },
-  '/UpdateRow': {
-    GET: async (request) => {
-      const { tableClient } = request.internal;
-      const { queries = {} } = request.req;
-      const params = [{ verifyCode: queries.code }, { verified: true }];
-      console.log(params);
-      await tableClient.updateRow({
-        tableName: 'test',
-        condition: {
-          rowExistenceExpectation: 0,
-        },
-        primaryKey: [{ id: queries.id }],
-        updateOfAttributeColumns: [{ PUT: params }],
-      });
-    },
+  'GET /UpdateRow': async (request) => {
+    const { tableClient } = request.internal;
+    const { queries = {} } = request.req;
+    const params = [{ verifyCode: queries.code }, { verified: true }];
+    console.log(params);
+    const data = await tableClient.updateRow({
+      tableName: 'test',
+      condition: {
+        rowExistenceExpectation: 0,
+      },
+      primaryKey: [{ id: queries.id }],
+      updateOfAttributeColumns: [{ PUT: params }],
+    });
+    return JSON.stringify(data, null, 2);
   },
 };
 
-const handler = middy(async (request) => {
-  return { body: JSON.stringify(request.req) };
-});
+const handler = middy(route);
 
-handler.use(tableStorePlugin()).use(jsonBodyParser()).use(formBodyParser()).use(routeParser(route));
+handler.use(tableStorePlugin()).use(jsonBodyParser()).use(formBodyParser());
 exports.initializer = handler.initializerHandler;
 
 exports.handler = handler;
