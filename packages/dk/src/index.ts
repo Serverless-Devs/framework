02da320dfceae723d6bcb04b31292a3c89beb3fa
huangfushan <243973775@qq.com>
2021-06-17 11:30:24 +0800

    feat: 🎸 dk add tablestore-event-parser

--- packages/dk/src/index.ts
+++ packages/dk/src/index.ts
@@ -1,4 +1,5 @@
 export { default as dk } from './dk';
+export { default as tablestoreEventParser } from '@serverless-devs/tablestore-event-parser';
 export { default as tablestoreInitialzerPlugin } from '@serverless-devs/tablestore-initialzer-plugin';
 export { default as validator } from '@serverless-devs/dk-validator';
 export { oss } from './oss';
