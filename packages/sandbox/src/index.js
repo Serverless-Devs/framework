39b6954b9836a90d56eba52e4cfb783f0e53806f
xsahxl <xsahxl@126.com>
2021-06-21 10:23:48 +0800

    feat: 🎸 npx nodemon

--- packages/sandbox/src/index.js
+++ packages/sandbox/src/index.js
@@ -1,3 +1,4 @@
 const { spawnSync } = require('child_process');
-
-spawnSync(`npm run server`, { shell: true, stdio: 'inherit' });
+const path = require('path');
+const filePath = path.resolve(__dirname, './sandbox.js');
+spawnSync(`npx nodemon -e html,css,js,ts,json ${filePath}`, { shell: true, stdio: 'inherit' });
