cbc75146372b43c5698f809d297c2b50afe46346
xsahxl <xsahxl@126.com>
2021-08-05 14:53:20 +0800

    feat: 🎸 scheduler

--- packages/dk/test/scheduler.test.ts
+++ packages/dk/test/scheduler.test.ts
@@ -3,10 +3,13 @@ import { scheduler } from '../src';
 describe('scheduler测试', () => {
   test('onScheduler 事件', async () => {
     scheduler.onScheduler({
+      handler: (ctx) => {
+        console.log(ctx.event);
+      },
       scheduler: {
         cronExpression: '0 0 8 * * *',
         enable: true,
-        payload: 'awesome-fc',
+        payload: 'serveless dk',
       },
     });
   });
