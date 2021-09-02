bd34d7d660a0ebea2a50f5dd017ec3cb650beaad
huangfushan <243973775@qq.com>
2021-09-02 18:02:33 +0800

    refactor: 💡 update http

--- packages/dk/src/http.ts
+++ packages/dk/src/http.ts
@@ -5,7 +5,7 @@ const Router = require("koa-router");
 const router = new Router()
 const app = new Koa();
 
-const http = (req, res, context) => serverless(app)(req, res, context);
+const http = (options) => (req, res, context) => serverless(app, options)(req, res, context);
 http.app = app;
 Object.setPrototypeOf(http, router);
 
