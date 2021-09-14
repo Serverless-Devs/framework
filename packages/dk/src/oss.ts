32b558d438e3a27acd4f299ad247a8844c2f03bb
huangfushan <243973775@qq.com>
2021-09-14 16:53:04 +0800

    refactor: 💡 update invalid code

--- packages/dk/src/oss.ts
+++ packages/dk/src/oss.ts
@@ -1,61 +1,61 @@
-import { isFcEnv, generateConfig } from './util';
-import dk from './dk';
+// import { isFcEnv, generateConfig } from './util';
+// import dk from './dk';
 
-interface IOnEvent {
-  bucketName: string;
-  events: string[];
-  filter: {
-    prefix: string;
-    suffix: string;
-  };
-}
+// interface IOnEvent {
+//   bucketName: string;
+//   events: string[];
+//   filter: {
+//     prefix: string;
+//     suffix: string;
+//   };
+// }
 
-interface IOnObject {
-  bucketName: string;
-  events?: string[];
-  filter: {
-    prefix: string;
-    suffix: string;
-  };
-}
-interface IOSSConfig<IOss> {
-  handler: Function | Object;
-  oss: IOss;
-}
+// interface IOnObject {
+//   bucketName: string;
+//   events?: string[];
+//   filter: {
+//     prefix: string;
+//     suffix: string;
+//   };
+// }
+// interface IOSSConfig<IOss> {
+//   handler: Function | Object;
+//   oss: IOss;
+// }
 
-const onEvent = (config: IOSSConfig<IOnEvent>) => {
-  if (isFcEnv) return dk(config.handler);
-  generateConfig('oss', config);
-  return dk(config.handler);
-};
+// const onEvent = (config: IOSSConfig<IOnEvent>) => {
+//   if (isFcEnv) return dk(config.handler);
+//   generateConfig('oss', config);
+//   return dk(config.handler);
+// };
 
-const onObjectCreated = (config: IOSSConfig<IOnObject>) => {
-  return onEvent({
-    ...config,
-    oss: {
-      events: ['oss:ObjectCreated:*'],
-      ...config.oss,
-    },
-  });
-};
+// const onObjectCreated = (config: IOSSConfig<IOnObject>) => {
+//   return onEvent({
+//     ...config,
+//     oss: {
+//       events: ['oss:ObjectCreated:*'],
+//       ...config.oss,
+//     },
+//   });
+// };
 
-const onObjectRemoved = (config: IOSSConfig<IOnObject>) => {
-  const defaultEvents = [
-    'oss:ObjectRemoved:DeleteObject',
-    'oss:ObjectRemoved:DeleteObjects',
-    'oss:ObjectRemoved:AbortMultipartUpload',
-  ];
-  return onEvent({
-    ...config,
-    oss: {
-      events: defaultEvents,
-      ...config.oss,
-    },
-  });
-};
+// const onObjectRemoved = (config: IOSSConfig<IOnObject>) => {
+//   const defaultEvents = [
+//     'oss:ObjectRemoved:DeleteObject',
+//     'oss:ObjectRemoved:DeleteObjects',
+//     'oss:ObjectRemoved:AbortMultipartUpload',
+//   ];
+//   return onEvent({
+//     ...config,
+//     oss: {
+//       events: defaultEvents,
+//       ...config.oss,
+//     },
+//   });
+// };
 
-export const oss = {
-  onEvent,
-  onObjectCreated,
-  onObjectRemoved,
-};
+// export const oss = {
+//   onEvent,
+//   onObjectCreated,
+//   onObjectRemoved,
+// };
--- packages/dk/src/scheduler.ts
+++ packages/dk/src/scheduler.ts
@@ -1,17 +1,17 @@
-import dk from './dk';
-import { isFcEnv, generateConfig } from './util';
+// import dk from './dk';
+// import { isFcEnv, generateConfig } from './util';
 
-interface ISchedulerConfig {
-  handler: Function | Object;
-  scheduler: { cronExpression: string; enable: boolean; payload: string };
-}
+// interface ISchedulerConfig {
+//   handler: Function | Object;
+//   scheduler: { cronExpression: string; enable: boolean; payload: string };
+// }
 
-const onScheduler = (config: ISchedulerConfig) => {
-  if (isFcEnv) return dk(config.handler);
-  generateConfig('scheduler', config);
-  return dk(config.handler);
-};
+// const onScheduler = (config: ISchedulerConfig) => {
+//   if (isFcEnv) return dk(config.handler);
+//   generateConfig('scheduler', config);
+//   return dk(config.handler);
+// };
 
-export const scheduler = {
-  onScheduler,
-};
+// export const scheduler = {
+//   onScheduler,
+// };
