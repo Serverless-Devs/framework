2b89a78da166b2c598fce19ebfac67173f47ed7e
huangfushan <243973775@qq.com>
2021-09-14 16:19:42 +0800

    refactor: 💡 update sandbox

--- packages/sandbox/src/sandbox.js
+++ packages/sandbox/src/sandbox.js
@@ -4,6 +4,7 @@ const core = require('@serverless-devs/core');
 const express = require('express');
 const { portIsOccupied } = require('@serverless-devs/dk-util');
 const app = express();
+const get = require('lodash.get');
 
 const sandbox = async () => {
   const args = process.env;
@@ -13,6 +14,7 @@ const sandbox = async () => {
   const currentPath = path.resolve(cwd);
   const spath = path.join(currentPath, './s.yml');
   const content = await core.getYamlContent(spath);
+
   if (!content) {
     throw new Error(`${cwd}路径下不存在s.[yaml|yml]文件`);
   }
@@ -28,6 +30,12 @@ const sandbox = async () => {
 
   const findKey = args.projectName || Object.keys(content.services)[0];
   const { props } = content.services[findKey];
+  const environmentVariables = get(props, 'function.environmentVariables', {});
+
+  // 添加环境变量
+  for (const i in environmentVariables) {
+    process.env[i] = environmentVariables[i]
+  }
   // todo 暂时用的是 devsapp/fc 组件，后续用jamstack-api 后 codeUri 需要改成 sourceCode
   const { codeUri } = props.function;
   fs.ensureSymlinkSync(
@@ -43,9 +51,13 @@ const sandbox = async () => {
       process.env.FC_FUNC_CODE_PATH = 'true';
       next();
     })
-    .use('/api', (req, res, ) => {
+    .use('/api', async (req, res) => {
       const fileModule = require(path.join(currentPath, '.s', codeUri, '/index'));
-      fileModule.handler(req, res, { context: { credentials } });
+      const context = { credentials };
+      if (fileModule.initializer) {
+        await fileModule.initializer(context, () => { });
+      }
+      fileModule.handler(req, res, context);
     });
   app.listen(port, () => {
     const url = `http://localhost:${port}/api`
