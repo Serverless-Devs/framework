1c4691a744136fa6b8558688f30a9e75621fd290
huangfushan <243973775@qq.com>
2021-09-14 16:38:45 +0800

    refactor: 💡 remove dk-deploy-common

--- packages/dk-deploy-common/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as generateTablestoreInitializer } from './generateTablestoreInitializer';
-export { default as generateSwaggerUI } from './generateSwaggerUI';
-export { default as generateGithub } from './generateGithub';
-export { default as getEnvs } from './getEnvs';
-export { default as generateFile } from './generateFile';
